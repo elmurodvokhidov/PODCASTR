de3bda1829f78756c0dffc65ea00b3a9a349c156
elmurodvokhidov <elmurodvkh@gmail.com>
2024-07-01 10:15:57 +0500

    displaying podcasts

--- convex/podcasts.ts
+++ convex/podcasts.ts
@@ -1,5 +1,5 @@
 import { ConvexError, v } from "convex/values";
-import { mutation } from "./_generated/server";
+import { mutation, query } from "./_generated/server";
 
 export const getUrl = mutation({
     args: {
@@ -49,5 +49,12 @@ export const createPodcast = mutation({
         });
 
         return podcast;
+    }
+});
+
+export const getTrendingPodcasts = query({
+    handler: async (ctx) => {
+        const podcasts = await ctx.db.query('podcasts').collect();
+        return podcasts;
     }
 });
