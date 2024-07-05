9418d88f98cd2086b96f94bc32ee05fe3c1316c4
elmurodvokhidov <elmurodvkh@gmail.com>
2024-07-05 12:36:38 +0500

    podcast player

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -3,6 +3,7 @@ import MobileNav from "@/components/MobileNav";
 import RightSidebar from "@/components/RightSidebar";
 import Image from "next/image";
 import { Toaster } from "@/components/ui/toaster";
+import PodcastPlayer from "@/components/PodcastPlayer";
 
 export default function RootLayout({
     children,
@@ -32,6 +33,7 @@ export default function RootLayout({
                 </section>
                 <RightSidebar />
             </main>
+            <PodcastPlayer />
         </div>
     );
 }
--- components/PodcastDetailPlayer.tsx
+++ components/PodcastDetailPlayer.tsx
@@ -5,12 +5,12 @@ import { useRouter } from "next/navigation";
 import { useState } from "react";
 
 import { api } from "@/convex/_generated/api";
-// import { useAudio } from '@/providers/AudioProvider';
 import { PodcastDetailPlayerProps } from "@/types";
 
 import LoaderSpinner from "./LoaderSpinner";
 import { Button } from "./ui/button";
 import { useToast } from "./ui/use-toast";
+import { useAudio } from "@/providers/AudioProvider";
 
 const PodcastDetailPlayer = ({
     audioUrl,
@@ -25,7 +25,7 @@ const PodcastDetailPlayer = ({
     authorId,
 }: PodcastDetailPlayerProps) => {
     const router = useRouter();
-    // const { setAudio } = useAudio();
+    const { setAudio } = useAudio();
     const { toast } = useToast();
     const [isDeleting, setIsDeleting] = useState(false);
     const deletePodcast = useMutation(api.podcasts.deletePodcast);
@@ -47,13 +47,13 @@ const PodcastDetailPlayer = ({
     };
 
     const handlePlay = () => {
-        // setAudio({
-        //     title: podcastTitle,
-        //     audioUrl,
-        //     imageUrl,
-        //     author,
-        //     podcastId,
-        // });
+        setAudio({
+            title: podcastTitle,
+            audioUrl,
+            imageUrl,
+            author,
+            podcastId,
+        });
     };
 
     if (!imageUrl || !authorImageUrl) return <LoaderSpinner />;
